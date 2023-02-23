5f3dd58a983f6e9899a69b7004901000f241a128
HuyLyMW <huylymw@gmail.com>
2023-02-23 22:25:33 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
-const contactsRouter =require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route");
+
+
+app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    return res.status(Error.statusCode || 500).json({
+        message: Error.message || "Internal Server Error",
+    });
+});
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +28,8 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
 
-app.use("/api/contacts",contactsRouter);
+app.use("/api/contacts", contactsRouter);
+
+
 
 module.exports = app;
